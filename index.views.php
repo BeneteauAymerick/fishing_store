74d59e473c256c39818a52d54eba99b882568959
BeneteauAymerick <47632571+BeneteauAymerick@users.noreply.github.com>
2019-02-15 10:03:35 -0400

    Add files via upload

---
+++ index.views.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Site de pêche">
+    <meta name="Benet972" content="">
+    <link rel="icon" href="Favicon/favicon.ico">
+
+    <title>Shore Jigging Martinique</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="CSS/css/bootstrap.min.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Shore Jigging Martinique</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Acceuil</a></li>
+            <li><a href="#about">Connexion</a></li>
+            <li><a href="register.php">Inscription</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Shore jigging</h1><br>
+        <p class="lead">Le shore jigging est pratiqué déjà depuis longtemps dans des pays comme les Etats-Unis,
+         l’Australie, la Nouvelle-Zélande, le Japon, etc. Plus près de nous, des pêcheurs le pratiquent avec succès
+          en Grèce, en Italie ou encore en Espagne.<br><br>Cette technique consiste en une récupération rapide de jigs
+         dans des fonds suffisamment importants se trouvant près du bord. Le leurre est ainsi manié dans toutes les
+          couches d’eaux afin d’intéresser toutes les espèces prédatrices, des poissons benthiques aux plus beaux
+         pélagiques.
+         </p><br>
+         <button class="btn btn">Connexion</button>
+         <a href="register.php">Inscription</a>
+
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="JS/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+   <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
